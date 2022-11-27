441bfa9e4d85fc6dfa51c573d94a8934224ccb6e
kacarervin <kacarervin@gmail.com>
2022-11-27 18:59:18 +0100

    Connect with Mongo DB

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ import { fileURLToPath } from "url";
 
 /* CONFIGURATION */
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); //we can grab file url
+const __dirname = path.dirname(__filename); //only when we use type "module"
 
 dotenv.config();
 const app = express();
@@ -24,3 +24,29 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
