c3eaa4aa985e03e92b406f5ffc38fba6007253cd
kacarervin <kacarervin@gmail.com>
2022-12-06 12:01:36 +0100

    Finish friends logic

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { fileURLToPath } from "url";
 
 /* CONFIGURATION */
@@ -27,6 +30,7 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 // import { verifyToken } from "./middleware/auth.js";
 
 /* FILE STORAGE */
@@ -45,11 +49,13 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES  */
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 
