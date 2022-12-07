2febfb59334fe70b6709781dae28fc0ca339e1e4
kacarervin <kacarervin@gmail.com>
2022-12-07 13:14:33 +0100

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homepage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 function App() {
-  return <div className="App">helloo</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
