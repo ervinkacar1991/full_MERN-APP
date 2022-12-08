246eada2aa7e5c639cafeeac97ae8185f3f69aec
kacarervin <kacarervin@gmail.com>
2022-12-08 09:13:59 +0100

    Start working with redux

---
+++ client/src/state/index.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+    },
+  },
+});
