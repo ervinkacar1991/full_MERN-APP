f9ee46225e3c91e7a6bcb3243d991781e106fd41
kacarervin <kacarervin@gmail.com>
2022-12-12 11:43:18 +0100

    Setup yup validation

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -16,8 +16,37 @@ import { setLogin } from "state";
 import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 
+const registerScheme = yup.object().shape({
+  firstName: yup().string.required("required"),
+  lastName: yup().string.required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  password: yup().string.required("required"),
+  location: yup().string.required("required"),
+  occupation: yup().string.required("required"),
+  picture: yup().string.required("required"),
+});
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup().string.required("required"),
+});
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
+
 const Form = () => {
-  return <div>Form</div>;
+  return <div>form</div>;
 };
 
 export default Form;
