b8cf516c0f198152b3384dde4c90af4d8fcbe2c2
kacarervin <kacarervin@gmail.com>
2022-12-19 11:07:42 +0100

    Finish functionality for likes

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -10,9 +10,38 @@ import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+likes = {
+  userId1: true,
+  userId2: true,
+};
+
+const PostWidget = ({
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+  const [isComments, setIsComments] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likCount = Object.keys(likes).length;
+
+  const { palette } = useTheme();
+
+  const main = palette.neutral.main;
+  const primary = palette.primary.main;
+
+  return <></>;
 };
 
 export default PostWidget;
