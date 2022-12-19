d374345dd208ddc12d7702fc658cac246da30a41
kacarervin <kacarervin@gmail.com>
2022-12-19 10:39:08 +0100

    Add friend part of postsWidget

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+
+const PostWidget = () => {
+  return <div>PostWidget</div>;
+};
+
+export default PostWidget;
